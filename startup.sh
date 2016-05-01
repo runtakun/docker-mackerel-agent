190b50a94519029b497bdd21c458dcadfe776bdc
Hiroyuki Shigeta <shigeta@glgl.org>
2016-05-01 13:20:49 +0900

    remove docker plugin support

--- startup.sh
+++ startup.sh
@@ -13,11 +13,6 @@ if [[ $auto_retirement ]]; then
     trap '/opt/mackerel-agent_linux_386/mackerel-agent retire -force' TERM KILL
 fi
 
-if [[ $enable_docker_plugin ]]; then
-    echo [plugin.metrics.docker] >>   /opt/mackerel-agent_linux_386/mackerel-agent.conf
-    echo command = \"/opt/mackerel-agent_linux_386/mackerel-plugin-docker -name-format name\" >>   /opt/mackerel-agent_linux_386/mackerel-agent.conf
-fi
-
 echo /opt/mackerel-agent_linux_386/mackerel-agent -apikey=${apikey} --conf=/opt/mackerel-agent_linux_386/mackerel-agent.conf $opts
 /opt/mackerel-agent_linux_386/mackerel-agent -apikey=${apikey} --conf=/opt/mackerel-agent_linux_386/mackerel-agent.conf $opts &
 wait ${!}
